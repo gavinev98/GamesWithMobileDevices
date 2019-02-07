837c58ed38b6a3d3df2fa589441f4abd5d97854b
Gavin Everett <gavin.everett48@hotmail.com>
2019-02-07 09:52:57 +0000

    Added Accelerometer, touchInput.

---
+++ Assets/BehavioursCam.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BehavioursCam : MonoBehaviour {
+
+	public float perspectiveZoomSpeed = 0.5f;        
+	public float orthoZoomSpeed = 0.5f;        
+	
+
+    // Use this for initialization
+    void Start () {
+      
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+
+    }
+}
---
+++ Assets/Object.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class Object : MonoBehaviour
+{
+    
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Touching the game object.
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Something Hit"); 
+            }
+        }
+        
+        // Accelerometer
+        transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+        
+       
+        
+
+
+    }
+}
