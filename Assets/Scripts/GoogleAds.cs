590ad6d268ab1a6c53287eef28d0d32872026ce8
gavinev98 <gavin.everett48@hotmail.com>
2019-03-18 12:55:14 +0000

    Added in Unity, Good admob ads.

---
+++ Assets/Scripts/GoogleAds.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+
+
+public class GoogleAds : MonoBehaviour
+{
+    // Banner View
+    private BannerView bannerView;
+    // InterstitialAd
+    private  InterstitialAd interstitial;
+
+    private RewardBasedVideoAd rewardBasedVideo;
+
+
+    public void Start()
+        {
+    #if UNITY_ANDROID
+                string appId = "ca-app-pub-9018596307029717~6800607028";
+    #elif UNITY_IPHONE
+                string appId = "ca-app-pub-9018596307029717~6800607028";
+    #else
+            string appId = "unexpected_platform";
+    #endif
+
+            // Initialize the Google Mobile Ads SDK.
+            MobileAds.Initialize(appId);
+
+            this.RequestBanner();
+
+        // Get singleton reward based video ad reference.
+        this.rewardBasedVideo = RewardBasedVideoAd.Instance;
+        this.RequestRewardBasedVideo();
+        this.RequestInterstitial();
+
+
+        //google video callbacks
+        // Called when an ad request has successfully loaded.
+        rewardBasedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        // Called when an ad request failed to load.
+        rewardBasedVideo.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+        // Called when an ad is shown.
+        rewardBasedVideo.OnAdOpening += HandleRewardBasedVideoOpened;
+        // Called when the ad starts to play.
+        rewardBasedVideo.OnAdStarted += HandleRewardBasedVideoStarted;
+        // Called when the user should be rewarded for watching a video.
+        rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        // Called when the ad is closed.
+        rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
+        // Called when the ad click caused the user to leave the application.
+        rewardBasedVideo.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+
+    }
+
+
+      
+
+    //Requesting banner.
+    private void RequestBanner()
+        {
+    #if UNITY_ANDROID
+                string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+    #elif UNITY_IPHONE
+                string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+    #else
+            string adUnitId = "unexpected_platform";
+    #endif
+
+            // Create a 320x50 banner at the top of the screen.
+            bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+            // Called when an ad request has successfully loaded.
+            bannerView.OnAdLoaded += HandleOnAdLoaded;
+            // Called when an ad request failed to load.
+            bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+            // Called when an ad is clicked.
+            bannerView.OnAdOpening += HandleOnAdOpened;
+            // Called when the user returned from the app after an ad click.
+            bannerView.OnAdClosed += HandleOnAdClosed;
+            // Called when the ad click caused the user to leave the application.
+            bannerView.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+    
+        // Create an empty ad request.  add a test device.
+            AdRequest request = new AdRequest.Builder().AddTestDevice(SystemInfo.deviceUniqueIdentifier).Build();
+
+            // Load the banner with the request.
+            bannerView.LoadAd(request);
+
+            bannerView.Destroy();
+
+        }
+
+        //Requesting interstitial ads.
+        private void RequestInterstitial()
+        {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+    #endif
+
+            // Initialize an InterstitialAd.
+            this.interstitial = new InterstitialAd(adUnitId);
+
+            // Called when an ad request has successfully loaded.
+            this.interstitial.OnAdLoaded += HandleOnAdLoaded1;
+            // Called when an ad request failed to load.
+            this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad1;
+            // Called when an ad is shown.
+            this.interstitial.OnAdOpening += HandleOnAdOpened1;
+            // Called when the ad is closed.
+            this.interstitial.OnAdClosed += HandleOnAdClosed1;
+            // Called when the ad click caused the user to leave the application.
+            this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication1;
+
+        // Create an empty ad request.  add a test device.
+            AdRequest request = new AdRequest.Builder().AddTestDevice(SystemInfo.deviceUniqueIdentifier).Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+
+        }
+
+
+    private void RequestRewardBasedVideo()
+    {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create an empty ad request. add a test device.
+        AdRequest request = new AdRequest.Builder().AddTestDevice(SystemInfo.deviceUniqueIdentifier).Build();
+        // Load the rewarded video ad with the request.
+        this.rewardBasedVideo.LoadAd(request, adUnitId);
+    }
+
+
+    public void showGoogleVideo()
+    {
+        if (this.rewardBasedVideo != null)
+            if (this.rewardBasedVideo.IsLoaded())
+            {
+                this.rewardBasedVideo.Show();
+            }
+            else
+            {
+                Debug.Log("The video ad has not sucessfully loaded");
+            }
+    }
+    
+      public void showInterstitialAd()
+         {
+        if (this.interstitial != null)
+        {
+            if (this.interstitial.IsLoaded())
+                this.interstitial.Show();
+        }
+        else
+        {
+            Debug.Log("The interstitial has not loaded sucessfully.");
+        }
+     }
+
+
+
+    //Handler events for banner views.
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdLoaded event received");
+        }
+
+        public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+        {
+            MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                                + args.Message);
+        }
+
+        public void HandleOnAdOpened(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdOpened event received");
+        }
+
+        public void HandleOnAdClosed(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdClosed event received");
+        }
+
+        public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdLeavingApplication event received");
+        }
+
+
+    //Handler events for Interstitals.
+        public void HandleOnAdLoaded1(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdLoaded event received");
+        }
+
+        public void HandleOnAdFailedToLoad1(object sender, AdFailedToLoadEventArgs args)
+        {
+            MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                                + args.Message);
+        }
+
+        public void HandleOnAdOpened1(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdOpened event received");
+        }
+
+        public void HandleOnAdClosed1(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdClosed event received");
+        }
+
+        public void HandleOnAdLeavingApplication1(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdLeavingApplication event received");
+        }
+
+
+    //google video
+
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLoaded event received");
+    }
+
+    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoFailedToLoad event received with message: "
+                             + args.Message);
+    }
+
+    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoOpened event received");
+    }
+
+    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoStarted event received");
+    }
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoClosed event received");
+       
+    }
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        string type = args.Type;
+        double amount = args.Amount;
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoRewarded event received for "
+                        + amount.ToString() + " " + type);
+      
+    }
+
+    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLeftApplication event received");
+        
+    }
+
+
+}
---
+++ Assets/Scripts/UnityAds.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using GoogleMobileAds.Api;
+using UnityEngine.Advertisements;
+
+public class UnityAds : MonoBehaviour
+{
+
+    // private BannerView bannerView;
+
+
+    //string for banner
+    public string bannerPlacement = "banner";
+    //setting testMode to true.
+    public bool testMode = true;
+
+    //google admob testing Identifiers.
+    [SerializeField] private string appID = "ca-app-pub-9018596307029717~6800607028";
+    [SerializeField] private string bannerID = "ca-app-pub-9018596307029717/3372621615";
+    [SerializeField] private string regularId = "ca-app-pub-9018596307029717/7594157403";
+
+
+
+    private void Start()
+    {
+
+        //google admobs initialization.
+        // MobileAds.Initialize((appID));
+
+        //unitybanner initialization will displayed after particular period of time.
+      //  StartCoroutine(ShowBannerWhenReady());
+
+
+
+    }
+
+    public void onClickShowBanner()
+    {
+        this.RequestBanner();
+    }
+
+    public void onClickShowRegAdd()
+    {
+        this.RequestRegAd();
+    }
+
+    public void RequestRegAd()
+    {
+        //  InterstitialAd regAd = new InterstitialAd(regularId);
+
+        //  AdRequest request = new AdRequest.Builder().Build();
+
+        //  regAd.LoadAd(request);
+    }
+
+
+    public void RequestBanner()
+    {
+        // bannerView = new BannerView(bannerID, AdSize.Banner, AdPosition.Center);
+
+        //  AdRequest request = new AdRequest.Builder().Build();
+
+        //  bannerView.LoadAd(request);
+    }
+
+
+    // Using unity ads
+
+    public void showUnityAd()
+    {
+
+        //if in the unity editor yield the game when the ad is running.
+#if UNITY_EDITOR
+        StartCoroutine(waitforAd());
+#endif
+        //Adding condition to check for stable connection
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show("video", new ShowOptions() { resultCallback = HandAdResult });
+        }
+    }
+
+    public void HandAdResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("Player has watched full ad");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Player has not watched full ad");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Advertisement has failed to load");
+                break;
+        }
+    }
+
+    //method to stop game when method is loaded
+    IEnumerator waitforAd()
+    {
+        float currentTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
+        yield return null;
+
+        while (Advertisement.isShowing)
+            yield return null;
+
+        Time.timeScale = currentTimeScale;
+    }
+
+
+    //method to show unity ads banner at a few seconds into the load of game.
+    /*
+    IEnumerator ShowBannerWhenReady()
+    {
+        // if banner is not ready wait few seconds
+        while (!Advertisement.IsReady("banner"))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(bannerPlacement);
+
+    }
+    */
+}
+
+
+//gamesfordevices2@gmail.com
